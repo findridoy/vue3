0b0c1ad3157017bbcfb18ab457e5049ef6c5febd
Ridoy Hossain <findridoy@gmail.com>
2022-01-03 15:32:49 +0600

    end of class 3

--- script.js
+++ script.js
@@ -2,16 +2,37 @@ const app = Vue.createApp({
     // template: `<h1> hello from {{name}}</h1>`,
     data() {
         return {
-            name: 'Nabhan',
-            age: 23,
-            fruitsShowing: false,
+            books: [
+                {
+                    title: 'the book thief',
+                    url: 'http://rokomari.com',
+                    isFav: false
+                },
+                {
+                    title: 'Javascript 101',
+                    url: 'http://rokomari.com',
+                    isFav: true
+                },
+                {
+                    title: 'Python Master',
+                    url: 'http://amazon.com',
+                    isFav: true
+                },
+                {
+                    title: 'Habluder Jonno Programming',
+                    url: 'http://amazon.com',
+                    isFav: false
+                },
+                {
+                    title: 'Programming er choddogosti',
+                    url: 'http://amazon.com',
+                    isFav: false
+                },
+            ]
         }
     },
 
     methods: {
-        changeName() {
-            this.name = 'Mohona'
-        }
     }
 
 
